86de548571466c98f5bc2d7596865e4d5aad4d44
Alex Hart <alex@hartcode.com>
2017-11-09 21:18:39 -0500

    Coins poping up no longer instantly collide with player

--- Assets/Scripts/Popup.cs
+++ Assets/Scripts/Popup.cs
@@ -11,28 +11,33 @@ public class Popup : MonoBehaviour {
     public float incre = 0.1f;
 
     public Vector3 InitialPosition;
-
+    private Collider collider;
 
     void Start()
     {
         //Store where we were placed in the editor
         InitialPosition = transform.position;
         Step = 0;
+        collider = gameObject.GetComponent<Collider>();
+        if (collider == null)
+        {
+            throw new MissingComponentException("Collider");
+        }
     }
 
     void FixedUpdate()
     {
         if (isEnabled)
         {
+            collider.enabled = false;
             Step += incre;
             //Make sure Steps value never gets too out of hand 
             if (Step < max)
             {
-
-                //Float up and down along the y axis, 
                 transform.position = new Vector3(InitialPosition.x, InitialPosition.y + (Mathf.Sin(Step) * Offset) + alt, InitialPosition.z);
             } else
             {
+                collider.enabled = true;
                 DestroyObject(this);
             }
         }
