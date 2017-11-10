703c86316c2640671f06b3f2c399f30a9b431581
Alex Hart <alex@hartcode.com>
2017-11-09 19:19:52 -0600

    Add Plant scripts and Coin popups to plants

---
+++ Assets/Scripts/PlantScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantScript : MonoBehaviour
+{
+    public GameObject Coin;
+    public Animator animator;
+
+    private bool empty = false;
+    public int coinCount = 1;
+    public int coinRadius = 5;
+    void Start()
+    {
+     
+        if (Coin == null)
+        {
+            throw new MissingReferenceException("Missing Coin Object");
+        }
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Body>() != null)
+        {
+            SnowBalled();
+        }
+    }
+
+    void SnowBalled()
+    {
+        SpawnCoin();
+        Animate();    
+    }
+    
+    void Animate()
+    {
+        animator.SetTrigger("Wave");
+    }
+
+    void SpawnCoin()
+    {
+        if (!empty)
+        {
+            
+                for (int i = 0; i < coinCount; i++)
+                {
+                    float radius = i * (365 / coinCount);
+                    Vector3 direction = Vector3.up;
+                    if (coinCount > 1)
+                    {
+                        direction = new Vector3(Mathf.Sin(radius), 0, Mathf.Cos(radius)) * coinRadius;
+                    }
+                    GameObject coin = GameObject.Instantiate(Coin);
+                    coin.transform.position = transform.position + direction;  // initial position
+
+                    coin.AddComponent<Popup>();
+                }
+           
+            empty = true;
+        }
+    }
+}
